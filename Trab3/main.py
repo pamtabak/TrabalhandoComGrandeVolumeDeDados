5d484a1ddb3ecd3965a73de46df6220d78c98fb4
Pamela Tabak <pamelatabak@poli.ufrj.br>
2016-11-03 21:08:38 -0200

    trab3: making pearson correlation in order to delete some useless info

--- Trab3/main.py
+++ Trab3/main.py
@@ -1,36 +1,40 @@
 import matplotlib as plt
 import numpy as np
 import csv
-
-#import math
-#import scipy.stats as stats
+import math
+import scipy.stats as stats
 
 # Target is the real class of the object (last attribute)
 # Target 1 is the less likely (and Target 0 the most likely)
 
-# Reading dataset
-csvFile   = open('dataset_trabalho3/train_file.csv', 'rb')
-csvReader = csv.reader(csvFile, delimiter=',')
+def readDataset (datasetName):
+	# Array of arrays. Each array is a column of the dataset
+	dataset = []
 
-# Array of arrays. Each array is a column of the dataset
-dataset = []
-header  = []
+	# Reading dataset
+	csvFile   = open(datasetName, 'rb')
+	csvReader = csv.reader(csvFile, delimiter=',')
 
-# Reading headers (so we know the amount of columns there are)
-row = csvReader.next()
-for column in row:
-	header.append(column)
-	dataset.append([])
-
-for row in csvReader:
-	col = 0
+	# Reading headers (so we know the amount of columns there are)
+	row = csvReader.next()
 	for column in row:
-		dataset[col].append(column)
-		col += 1
+		dataset.append([])
+
+	for row in csvReader:
+		col = 0
+		for column in row:
+			dataset[col].append(float(column))
+			col += 1
+
+	return dataset
+
+
+trainingDataset = readDataset('dataset_trabalho3/train_file.csv')
+testingDataset  = readDataset('dataset_trabalho3/test_file.csv')
 
 # Checking what we can do with each column
 # 1. If there is any column that doesn`t make any difference (for instance, all the records are filled with the same value)
-for column in dataset:
+for column in trainingDataset:
 	allElementsEqual = True
 	value = column[0]
 	for x in column:
@@ -38,5 +42,17 @@ for column in dataset:
 			allElementsEqual = False
 			break
 	if (allElementsEqual):
-		dataset.remove(column)
+		trainingDataset.remove(column)
 
+
+#2. If there are any 2 columns correlated
+eps   = 0.05
+for column1 in trainingDataset:
+	sameColumns = False
+	for column2 in trainingDataset:
+		# Just checking if the columns are not the same
+		if (np.array_equal(column1, column2) == False):
+			correlation = stats.pearsonr(column1, column2)[0]
+			if (math.fabs(correlation) + eps >= 1):
+				print(len(trainingDataset))
+				trainingDataset.remove(column2)
