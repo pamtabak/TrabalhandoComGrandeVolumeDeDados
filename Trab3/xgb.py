59a92db88091f5009eff6c01eb075ead9382d965
Pamela Tabak <pamelatabak@poli.ufrj.br>
2016-12-07 21:33:13 -0200

    minor changes

--- Trab3/xgb.py
+++ Trab3/xgb.py
@@ -3,7 +3,7 @@ import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
-print("1")
+print("loading the data")
 # Load the data
 train_df = pd.read_csv('dataset_trabalho3/train_file.csv', header=0)
 test_df  = pd.read_csv('dataset_trabalho3/test_file.csv',  header=0)
@@ -12,23 +12,22 @@ header = list(train_df.columns.values)
 
 complete_dataframe = train_df[header[:-1]].append(test_df[header[:-1]])
 
-print("2")
 train_X = complete_dataframe[0:train_df.shape[0]].as_matrix()
 test_X  = complete_dataframe[train_df.shape[0]::].as_matrix()
 train_y = train_df['TARGET']
 
-print("3")
-#127,0.0522649127,5
+print("classifing")
 gbm = xgb.XGBClassifier(max_depth=5, n_estimators=127, learning_rate=0.0522649127).fit(train_X, train_y)
+print("predicting")
 predictions = gbm.predict_proba(test_X)
-print("4")
 
+print("cleaning results")
+#We only want the probability of being from class 1
 result = []
 for i in xrange(0, len(predictions)):
 	print(predictions[i][1])
 	result.append(predictions[i][1])
 
-# submission = pd.DataFrame({ 'TARGET': predictions })
-# submission.to_csv("submission.csv", index=False)
+print("saving results")
 np.savetxt('resultados/xgb_result.csv', np.array(result), fmt="%f", delimiter=",")
-print("5")
+print("finished")
